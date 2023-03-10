ac6eb5ef4735145ae1b146002a3eb9e43533bf04
Nikitha <nikithaseela11@gmail.com>
2023-03-10 16:56:29 +0530

    hero page initial commit

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../components/HeroPage.module.css';
+import styles from '../components/HeroPage.module.css'
 import image1 from "../components/images/1.png"
 import image2 from "../components/images/2.png"
 import image3 from "../components/images/3.png"
@@ -11,7 +11,7 @@ function HeroPage(){
             <div className={`${styles.box1}`}>
             <div className={`${styles.loginContainer}`}>
             <div className={`${styles.image}`}>
-                <img className="a" src={image1} alt="bank"></img>
+                <img className={`${styles.a}`} src={image1} alt="bank"></img>
             </div>
             <div className={`${styles.content}`}>
                     <h4>FASTER</h4>
@@ -21,7 +21,7 @@ function HeroPage(){
             </div>
             <div className={`${styles.loginContainer2}`}>
                 <div className={`${styles.image}`}>
-                    <img className="b" src={image2} alt="transaction"></img>
+                    <img className={`${styles.b}`} src={image2} alt="transaction"></img>
                 </div>
             <div className={`${styles.content}`}>
                     <h4>EASIER</h4>
@@ -32,7 +32,7 @@ function HeroPage(){
             </div>
             <div className={`${styles.box2}`}>
                 <div className={`${styles.image}`}>
-                    <img className="c" src={image3} alt="fasterPayments"></img>
+                    <img className={`${styles.c}`} src={image3} alt="fasterPayments"></img>
                 </div>
             <div className={`${styles.box2Content}`}>
                     <h4>SMARTER</h4>
